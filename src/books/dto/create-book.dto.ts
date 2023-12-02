2caf64cde205fb7103eabbe6f1171ab54c270f13
Fannn03 <fann.fann771@gmail.com>
2023-12-02 12:14:33 +0700

    feat: add validation release year

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty, IsNumber, IsNumberString, Max, Min } from "class-validator";
+import { IsNotEmpty, IsNumber, IsNumberString, Max, Min, Validate } from "class-validator";
+import { ReleaseYearValidation } from "../validation/release-year.validation";
 
 export class CreateBookDto {
   @IsNotEmpty()
@@ -13,6 +14,9 @@ export class CreateBookDto {
 
   @IsNotEmpty()
   @IsNumberString()
+  @Validate(ReleaseYearValidation, [1980, 2021], {
+    message: "release_year cannot less than 1980 or greater than 2021"
+  })
   release_year: number;
 
   @IsNotEmpty()
